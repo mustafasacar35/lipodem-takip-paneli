d40b6772c5c7e0ed14e9a6f89cb548b0d237ac75
mustafasacar35 <mustafasacar@hotmail.com>
2025-07-15 13:22:43 +0300

    Add files via upload

--- hasta_yonetim.js
+++ hasta_yonetim.js
@@ -1,5 +1,6 @@
 
 let hastalar = [];
+let arsivGorunumu = false;
 
 function kaydetJSON() {
   localStorage.setItem("hastalar", JSON.stringify(hastalar));
@@ -9,6 +10,7 @@ function yukleJSON() {
   const kayitli = localStorage.getItem("hastalar");
   if (kayitli) {
     hastalar = JSON.parse(kayitli);
+    guncelleListe();
   } else {
     fetch("hastalar.json")
       .then(res => res.json())
@@ -18,6 +20,10 @@ function yukleJSON() {
         guncelleListe();
       });
   }
+}
+
+function arsivModu(aktifMi) {
+  arsivGorunumu = aktifMi;
   guncelleListe();
 }
 
@@ -28,7 +34,7 @@ function guncelleListe() {
   const arama = document.getElementById("arama").value.toLowerCase();
 
   hastalar
-    .filter(h => !h.arsiv && h.isim.toLowerCase().includes(arama))
+    .filter(h => h.arsiv === arsivGorunumu && h.isim.toLowerCase().includes(arama))
     .forEach((hasta, index) => {
       const kart = template.content.cloneNode(true);
       kart.querySelector(".isim").innerText = hasta.isim;
@@ -59,11 +65,17 @@ function duzenleHasta(index) {
 }
 
 function arsivleHasta(index) {
-  if (confirm("Bu hasta arşive alınsın mı?")) {
-    hastalar[index].arsiv = true;
-    kaydetJSON();
-    guncelleListe();
+  if (arsivGorunumu) {
+    if (confirm("Bu hasta arşivden çıkarılsın mı?")) {
+      hastalar[index].arsiv = false;
+    }
+  } else {
+    if (confirm("Bu hasta arşive alınsın mı?")) {
+      hastalar[index].arsiv = true;
+    }
   }
+  kaydetJSON();
+  guncelleListe();
 }
 
 window.onload = yukleJSON;
