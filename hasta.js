6618c972b679d05c6288883096a45b899968f55d
mustafasacar35 <mustafasacar@hotmail.com>
2025-07-15 12:20:44 +0300

    Add files via upload

--- hasta.js
+++ hasta.js
@@ -19,7 +19,18 @@ fetch('hastalar.json')
     for (let i = 1; i <= hasta.hafta; i++) {
       const el = document.createElement('div');
       el.className = 'hafta-kutu';
-      el.innerText = `Hafta ${i}`;
+      el.innerHTML = `
+        <h3>Hafta ${i}</h3>
+        <p><a href="data/${hasta.id}/hafta_${i}/liste.pdf" target="_blank">📄 PDF Listesini Görüntüle</a></p>
+        <p>🖼 Tarif Kartları:</p>
+        <div class="kartlar">
+          <img src="data/${hasta.id}/hafta_${i}/kartlar/enginar.jpg" alt="Enginar" width="100" />
+          <img src="data/${hasta.id}/hafta_${i}/kartlar/yumurta.jpg" alt="Yumurta" width="100" />
+        </div>
+        <p>📝 Haftalık Mesaj:</p>
+        <textarea rows="4" style="width:100%;">Bu hafta başlangıç yapıldı...</textarea>
+        <br><button onclick="alert('ZIP oluşturma ileride eklenecek')">📦 ZIP OLUŞTUR</button>
+      `;
       haftalarDiv.appendChild(el);
     }
   });
