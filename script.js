529b4829a84530118282c81a04b9e3f911bc0934
mustafasacar35 <mustafasacar@hotmail.com>
2025-07-15 14:04:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hazır!');
+const hastaListesi = document.getElementById("hastaListesi");
+if (hastaListesi) {
+  hastaListesi.innerHTML = '<a href="hasta.html?id=hatice_hanim">Hatice Hanım (5. hafta)</a>';
+}
+
+function yeniHaftaEkle() {
+  const haftalar = document.getElementById("haftalar");
+  const yeniHafta = document.createElement("div");
+  yeniHafta.innerHTML = "<h3>Yeni Hafta Eklendi</h3>";
+  haftalar.appendChild(yeniHafta);
+}
